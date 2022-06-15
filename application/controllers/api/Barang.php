7122d9cc21ddb977629a4ba94b968985a22fb309
Alfon5000 <alfonlai97@gmail.com>
2022-06-15 11:34:33 +0700

    Update Barang.php & Barang_model.php

--- application/controllers/api/Barang.php
+++ application/controllers/api/Barang.php
@@ -14,4 +14,97 @@ class Barang extends REST_Controller
 		parent::__construct();
 		$this->load->model('Barang_model', 'barang');
 	}
+
+	public function index_get()
+	{
+		$kode = $this->get('kode');
+		if ($kode === null) {
+			$barang = $this->barang->getBarang();
+		} else {
+			$barang = $this->barang->getBarang($kode);
+		}
+
+		if ($barang) {
+			$this->response([
+				'status' => true,
+				'data' => $barang
+			], REST_Controller::HTTP_OK);
+		} else {
+			$this->response([
+				'status' => false,
+				'data' => 'kode not found'
+			], REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+
+	public function index_delete()
+	{
+		$kode = $this->delete('kode');
+
+		if ($kode === null) {
+			$this->response([
+				'status' => false,
+				'message' => 'Provide a kode'
+			], REST_Controller::HTTP_BAD_REQUEST);
+		} else {
+			if ($this->barang->deleteBarang($kode) > 0) {
+				$this->response([
+					'status' => true,
+					'kode' => $kode,
+					'message' => 'Data Barang has been deleted!'
+				], REST_Controller::HTTP_NO_CONTENT);
+			} else {
+				$this->response([
+					'status' => false,
+					'message' => 'kode not found'
+				], REST_Controller::HTTP_BAD_REQUEST);
+			}
+		}
+	}
+
+	public function index_post()
+	{
+		$data = [
+			'kode' => $this->post('kode'),
+			'nama_barang' => $this->post('nama_barang'),
+			'jenis' => $this->post('jenis'),
+			'harga' => $this->post('harga'),
+			'stok' => $this->post('stok')
+		];
+		if ($this->barang->createBarang($data) > 0) {
+			$this->response([
+				'status' => true,
+				'message' => 'new barang has been created.'
+			], REST_Controller::HTTP_CREATED);
+		} else {
+			$this->response([
+				'status' => false,
+				'message' => 'failed to create new data!'
+			], REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+
+	public function index_put()
+	{
+		$kode = $this->put('kode');
+		$data = [
+			'kode' => $this->put('kode'),
+			'nama_barang' => $this->put('nama_barang'),
+			'jenis' => $this->put('jenis'),
+			'harga' => $this->put('harga'),
+			'stok' => $this->put('jurusan')
+		];
+
+		if ($this->barang->updateBarang($data, $kode) > 0) {
+			$this->response([
+				'status' => true,
+				'message' => 'data barang has been updated.'
+			], REST_Controller::HTTP_NO_CONTENT);
+		} else {
+			$this->response([
+				'status' => false,
+				'message' => 'failed to update new data!'
+			], REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
 }
---
+++ application/models/Barang_model.php
@@ -0,0 +1,27 @@
+<?php
+class Barang_model extends CI_Model
+{
+	public function getBatang($kode = null)
+	{
+		if ($kode === null) {
+			return $this->db->get('barang')->result_array();
+		} else {
+			return $this->db->get_where('barang', ['kode' => $kode])->result_array();
+		}
+	}
+	public function deleteBarang($kode)
+	{
+		$this->db->delete('barang', ['kode' => $kode]);
+		return $this->db->affected_rows();
+	}
+	public function createBarang($data)
+	{
+		$this->db->insert('barang', $data);
+		return $this->db->affected_rows();
+	}
+	public function updateBarang($data, $kode)
+	{
+		$this->db->update('barang', $data, ['kode' => $kode]);
+		return $this->db->affected_rows();
+	}
+}
